45bfb22e5fae789313656f5f558b0f7d7a45eab0
Janet <vongjanet@yahoo.com>
2018-06-19 23:34:39 -0700

    Use PORT from environment.

--- lesson13/BookmarkServer.py
+++ lesson13/BookmarkServer.py
@@ -154,6 +154,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
